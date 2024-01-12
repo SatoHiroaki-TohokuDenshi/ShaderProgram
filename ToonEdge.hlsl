9ebf39bf156b59ed43b2cb2b335228f73c973bc2
央旺 佐藤 <220511@jc-21.jp>
2024-01-12 11:23:07 +0900

    余分な部分を削除

--- ToonEdge.hlsl
+++ ToonEdge.hlsl
@@ -1,12 +1,4 @@
 //───────────────────────────────────────
-// テクスチャ＆サンプラーデータのグローバル変数定義
-//───────────────────────────────────────
-Texture2D		g_texture : register(t0);	//テクスチャー
-SamplerState	g_sampler : register(s0);	//サンプラー
-
-Texture2D		g_toon_texture : register(t1);
-
-//───────────────────────────────────────
 // コンスタントバッファ
 // DirectX 側から送信されてくる、ポリゴン頂点以外の諸情報の定義
 //───────────────────────────────────────
@@ -22,22 +14,12 @@ cbuffer gModel : register(b0)
 	bool		isTextured;		// テクスチャーが貼られているかどうか
 };
 
-cbuffer gModel : register(b1)
-{
-	float4		lightPos;		// 光源の位置
-	float4		eyePos;			// 視点
-};
-
 //───────────────────────────────────────
 // 頂点シェーダー出力＆ピクセルシェーダー入力データ構造体
 //───────────────────────────────────────
 struct VS_OUT
 {
 	float4 pos  : SV_POSITION;	// 位置
-	float2 uv	: TEXCOORD;		// UV座標
-	float4 eyeDir	: POSITION;	// 視点ベクトル
-	float4 normal	: NORMAL;	// 法線
-	float4 color	: COLOR;	// 色（明るさ）
 };
 
 //───────────────────────────────────────
@@ -45,32 +27,17 @@ struct VS_OUT
 //───────────────────────────────────────
 VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 {
-	//ピクセルシェーダーへ渡す情報
-	VS_OUT outData = (VS_OUT)0;
+	VS_OUT outData;
 
+	// 法線の平行移動の情報を削除
 	normal.w = 0;
 
-	//ローカル座標に、ワールド・ビュー・プロジェクション行列をかけて
-	//スクリーン座標に変換し、ピクセルシェーダーへ
+	// 法線方向に拡大
 	pos = pos + normal * 0.05;
+
+	// スクリーン座標に変換
 	outData.pos = mul(pos, matWVP);
 
-	// 法線を変形
-	normal = normalize(mul(normal, matNormal));
-	outData.normal = normal;
-
-	// UV座標はそのまま
-	outData.uv = uv.xy;
-
-	// 光源の位置を正規化
-	float4 light = normalize(lightPos);
-	outData.color = saturate(dot(normal, light));
-
-	// 視点ベクトルを獲得
-	float4 posWorld = mul(pos, matWorld);
-	outData.eyeDir = eyePos - posWorld;
-
-	//まとめて出力
 	return outData;
 }
 
@@ -79,34 +46,6 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 //───────────────────────────────────────
 float4 PS(VS_OUT inData) : SV_Target
 {
+	// 黒を出力
 	return (0.0, 0.0, 0.0, 0.0);
-
-	float4 lightColor = float4(1.0, 1.0, 1.0, 1.0);		// 光源の色(強さ)
-
-	float4 diffuse;		// 拡散反射
-	float4 ambient;		// 環境光
-	float4 specular;	// 鏡面反射
-
-	// 階調変換
-	float2 uv;
-	uv.x = inData.color.r;
-	uv.y = abs(dot(inData.normal, normalize(inData.eyeDir)));
-	return g_toon_texture.Sample(g_sampler, uv);
-
-	if (isTextured) {
-		diffuse = lightColor * g_texture.Sample(g_sampler, inData.uv) * g_toon_texture.Sample(g_sampler, uv);
-		ambient = lightColor * g_texture.Sample(g_sampler, inData.uv) * ambinetColor;
-	}
-	else {
-		diffuse = lightColor * diffuseColor * g_toon_texture.Sample(g_sampler, uv);
-		ambient = lightColor * diffuseColor * ambinetColor;
-	}
-
-	float4 nLight = dot(inData.normal, normalize(lightPos));
-	float4 ref = normalize(2 * nLight * inData.normal - normalize(lightPos));
-	specular = pow(saturate(dot(ref, normalize(inData.eyeDir))), shininess) * specularColor;
-
-
-	return (diffuse + ambient);
-	//return (diffuse + ambient + specular);
 }
